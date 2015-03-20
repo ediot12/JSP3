e4bd5c477ba6bfa50366a0f1fc8d24b8eb1ca68e
301 <301@223.194.169.122>
2015-03-20 16:43:10 +0900

    ds

---
+++ 02.FirstProject/src/exam/first/HelloServlet.java
@@ -0,0 +1,77 @@
+package exam.first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//한글처리
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+
+		out.print("<html>");
+		out.print("이름 :" + name + "<br>");
+		out.print("ID : " + id +"<br>" );
+		out.print("암호 : " + pwd +"<br>" );
+		out.print("</html>");
+		
+		
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		out.print("<html>");
+		out.print("이름 :" + name + "<br>");
+		out.print("ID : " + id +"<br>" );
+		out.print("암호 : " + pwd +"<br>" );
+		out.print("</html>");
+		
+		
+	}
+
+}
